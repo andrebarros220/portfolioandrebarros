c414ae2167f31c9225e29c2505c527b0db60ecb4
andre barros <andrecamilobarros@yahoo.com.br>
2020-01-28 18:19:02 -0300

    Meu Primeiro Commit - Meu portfolio Spring

---
+++ src/main/java/br/com/meuportfolio/MeuPortfolioApplication.java
@@ -0,0 +1,13 @@
+package br.com.meuportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/meuportfolio/MeuPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meuportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
